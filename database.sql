18b9b551ea53f8628d1a0b040244eb7b09323079
Alexand-p <3082303@gmail.com>
2023-01-12 00:07:30 +0300

    added git prj

---
+++ database.sql
@@ -0,0 +1,13 @@
+
+-- База данных: `test`
+-- Структура таблицы `users`
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `full_name` varchar(355) DEFAULT NULL,
+  `login` varchar(100) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(500) DEFAULT NULL,
+  `avatar` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
